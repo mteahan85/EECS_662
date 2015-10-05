bef944a3d29efcd7f950924076fb65ee004c6dff
Megan Teahan <mteahan@ku.edu>
2015-10-05 14:51:17 -0500

    Parser working for Project 2 exercise 1

--- Project_02/EECS_662_Project_2_Exercise_1.rkt
+++ Project_02/EECS_662_Project_2_Exercise_1.rkt
@@ -21,12 +21,7 @@
   (mtsub)
   (aSub (name symbol?) (value CFAE?) (ds DefrdSub?))
   )
-
-#! Binding
-(define-type binding
-  (binded (name symbol?) (bind_expr CFAE?))
- )
-
+             
 #! Binop Record 
 (define-type binop-rec
   (binopr (name symbol?) (op procedure?)))
@@ -72,7 +67,7 @@
                (op `div)
                (parse-cfae (cadr s_expr))
                (parse-cfae (caddr s_expr))))
-         ((fun) (fun (parse-cfae (cadr s_expr))
+         ((fun) (fun (cadr s_expr)
                      (parse-cfae (caddr s_expr))))
          ((app) (app (parse-cfae (cadr s_expr))
                      (parse-cfae (caddr s_expr))))
@@ -86,128 +81,56 @@
     )
   )
 
-#! parses the bindings for with and puts them into a list
-(define parse-bindings
-  (lambda (binds)
-    (if (symbol? (car binds))
-        (list (parse-bind binds))
-        (map parse-bind binds)
-        )
-    )
-  )
-
-#! puts all binding information in a binding type
-(define parse-bind
-  (lambda (bind)
-    (binded (car bind) (parse-cfae (cadr bind)))
-    )
-  )
-        
                  
 #! evaluates input
 (define eval-cfae
   (lambda (s_expr)
-    (interp-cfae (parse-cfae s_expr))
+    (interp-cfae (parse-cfae s_expr)) ;; in excerise 2 call elab before interpreter 
     )
   )
 
-#! checks to make sure there are no duplicate identifiers in the bindings
-(define no-copies? 
-  (lambda (bindings)
-    (let loop ((b (car bindings)) (bs (cdr bindings)))
-      (cond 
-        ((empty? bs) true)
-        ((no-copies-helper b bs) false)
-        (else (loop (car bs) (cdr bs)))
-        )
-      )
-    )
-  )
-
-#! checks a current identifier against the other bindings in the list to check for copies
-(define no-copies-helper
-  (lambda (b bs)
-    (cond 
-      ((empty? bs) false)
-      ((symbol=? (binded-name b) (binded-name (car bs))) true)
-      (else (no-copies-helper b (cdr bs)))
-      )
-    )
-  )
+;;excerise 2
+;; two grammaers 1. WCFAE 2.CFAE. Elab converts WCFAE to CFAE. WFAE: with => CFAE: app. WFAE: cond0 => CFAE: if0. No parsing. WCFAE is an abstract syntax.
 
 #! interpreter 
 (define interp-cfae
   (lambda (cfae ds)
     (type-case CFAE cfae
-      (num (n) n)
-      (binop (bo l r) ((lookup (op-name bo) binop-table) (interp-cfae l) (interp-cfae r)))
+      (num (n) (num n))
+      (binop (bo l r) (num ((lookup (op-name bo) binop-table) (num-n (interp-cfae l)) (num-n (interp-cfae r)))))
       (app (fun_expr arg_expr)
-           (let ([fun_val (interp-cfae fun_expr)])
-             (interp-cfae (subst-cfae (fun-body fun_val)
-                                 (fun-id fun_val)
-                                 (interp-cfae arg_expr)))))
-      (fun (id body) (interp-cfae (subst-cfae 
+           (let ([fun_val (interp-cfae fun_expr ds)])
+             ;check if fun val is a function
+             (interp-cfae (fun-body fun_val)
+                          (aSub (fun-id fun_val)
+                                (interp-cfae arg_expr ds)
+                                ds))))
+      (if0 (c t e ) (if (eq? (num 0) (interp-cfae c ds))
+                        (interp-cfae t)
+                        (interp-cfae e)))
+      (fun (id body) (fun id body))
       (op (o) o)
-      (id (v) (error 'interp-cfae "free identifier"))
+      (id (v)(lookup-ds v ds))
       )
     )
   )
 
-#! 
-(define subst-cfae
-  (lambda (bindings body)
-    (foldl unbind body bindings)
-    )
-  )
-
-#! Breaks down the binding to be able to put through the subst
-(define unbind
-  (lambda (bind body)
-    (subst body (binded-name bind) (num (interp-cfae (binded-bind_expr bind))))
-    )
-  )
-
-#! check ids to see if they match or not
-(define check-ids 
-  (lambda (sub_id bindings)
-    (not (andmap (lambda (bind)
-              (if (symbol=? sub_id (binded-name bind))
-                  false
-                  true
-                  )
-              ) bindings)
-    )
-    )
-  )
-
-#! gets binding informatoin to pass onto subst
-(define subst-bindings 
-  (lambda (sub_id val bindings)
-    (map (lambda (bind)
-           (binded (binded-name bind) (subst (binded-bind_expr bind) sub_id val)))
-         bindings)
-    )
-  )
-            
-       
-(define subst
-  (lambda (expr sub_id val)
-    (type-case WAEE expr
-      (num (n) (num n))
-      (binop (bo l r) (binop bo
-                             (subst l sub_id val)
-                             (subst r sub_id val)))
-      (with (bindings bound_body)
-            (if (check-ids sub_id bindings) 
-                (with (subst-bindings sub_id val bindings) bound_body)
-                (with (subst-bindings sub_id val bindings) (subst bound_body sub_id val))
+#! Looks up variable in DefrdSub list      
+(define lookup-ds
+  (lambda (name ds)
+    (type-case DefrdSub ds
+      (mtsub () (error 'lookup "undefined id"))
+      (aSub (bound_name bound_value rest_ds)
+            (if (symbol=? name bound_name)
+                bound_value
+                (lookup-ds name rest_ds)
                 )
             )
-      (id (v) 
-          (if (symbol=? v sub_id)
-              val
-              (id v)))
-      (op (o) (op o))
       )
     )
   )
+             
+      ;(app (fun 'x (binop (op 'add) (num 1) (id 'x))) (num 3))      
+(parse-cfae '(app (fun x (+ 1 x)) 3))
+(eval-cfae '(app (fun x (+ 1 x)) 3))
+;(interp-cfae '(app (fun 'x (binop (op 'add) (num 1) (id 'x))) (num 3)))
